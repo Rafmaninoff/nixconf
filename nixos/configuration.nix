4ee375621644b3d3722f5f8e5fb9e9ca120da1ea
Rafael Uria <rafmaninoff@gmail.com>
2024-03-13 10:58:51 -0300

    switch to plasma 6, experiment

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -81,7 +81,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 
   programs.hyprland = {
     enable = true;
