cd75229574dcc1b124635856e9ce4c1ec34e90d9
Rafael Uria <rafmaninoff@gmail.com>
2024-04-18 18:28:32 -0300

    update flake inputs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,8 +76,6 @@ in
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm = { enable = true; wayland.enable = true; };
   services.desktopManager.plasma6.enable = true;
 
