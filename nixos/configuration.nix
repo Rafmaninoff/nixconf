ba07c59b66ba87b49d0711e2b85e38f12ba8d90c
Rafael Uria <rafmaninoff@gmail.com>
2024-06-19 00:16:59 -0300

    modularise ssh, enable firewall and configure fail2ban so I don't get attempted logins every 5 seconds.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,6 +16,7 @@ in
       ./fonts.nix
       ./sudo.nix
       ./sops.nix
+      ./ssh.nix
       ./openrgb.nix
       modules/duckdns.nix
     ];
@@ -278,20 +279,14 @@ in
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh =
-    {
-      enable = true;
-      openFirewall = true;
-    };
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
+  };
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -1,7 +1,32 @@
 { pkgs, config, lib, ... }:
 {
-  services.endlessh = {
+  services.fail2ban = {
     enable = true;
-    port = 2222;
+    maxretry = 3;
+    ignoreIP = [ "192.168.1.0/23" ];
+    bantime = "24h";
+    bantime-increment = {
+      enable = true;
+      formula = "bantime * math.exp(float(ban.Count + 1) * banFactor)/math.exp(1*banFactor)";
+      factor = "1";
+      rndtime = "8m";
+      overalljails = true;
+    };
+    jails.sshd.settings = {
+      mode = "aggresive";
+    };
   };
+
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    openFirewall = true;
+    settings = {
+      PasswordAuthentication = true;
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "no";
+    };
+  };
+
 }
