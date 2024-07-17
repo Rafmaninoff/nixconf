c87b4bf91404c7965cb6cea165547e07bb939422
Rafael Uria <rafmaninoff@gmail.com>
2024-07-17 18:25:19 -0300

    kitty tweaks

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -10,6 +10,15 @@
       disable_ligatures = "never";
       enable_audio_bell = "yes";
 
+      #I don't want to confirm closing.
+      confirm_os_window_close = 0;
+
+      strip_trailing_spaces = "smart";
+
+      placement_strategy = "center";
+
+      window_padding_width = 2;
+
     };
     theme = "Tokyo Night";
   };
