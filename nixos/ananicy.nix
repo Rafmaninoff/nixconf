24d12ab45fcbb510cce92f00cf908d1d357b1fd3
Rafael Uria <rafmaninoff@gmail.com>
2024-07-30 18:04:12 -0300

    cgroups v2 move to place

--- nixos/ananicy.nix
+++ nixos/ananicy.nix
@@ -1,5 +1,8 @@
 { config, inputs, pkgs, pkgs-stable, ... }:
 {
+  #systemd now refuses to boot with cgroups v1
+  systemd.enableUnifiedCgroupHierarchy = true;
+
   services.ananicy = {
     enable = true;
     package = inputs.nixpkgs-stable.legacyPackages.x86_64-linux.ananicy-cpp;
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -243,8 +243,6 @@ in
     btrfs-assistant
   ]);
 
-  systemd.enableUnifiedCgroupHierarchy = true;
-
   systemd.user.services = {
     arrpc = {
       partOf = [ "graphical-session.target" ];
