18143f3977271e03fabe68efe1363314a29dc0ed
Rafael Uria <rafmaninoff@gmail.com>
2024-08-28 23:41:59 -0300

    actually remove it from the other end

--- hosts/raf-x570/configuration.nix
+++ hosts/raf-x570/configuration.nix
@@ -47,19 +47,6 @@ in
   # Select internationalisation properties.
 
   i18n = {
-    defaultLocale = "en_US.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "es_UY.UTF-8";
-      LC_IDENTIFICATION = "es_UY.UTF-8";
-      LC_MEASUREMENT = "es_UY.UTF-8";
-      LC_MONETARY = "es_UY.UTF-8";
-      LC_NAME = "es_UY.UTF-8";
-      LC_NUMERIC = "es_UY.UTF-8";
-      LC_PAPER = "es_UY.UTF-8";
-      LC_TELEPHONE = "es_UY.UTF-8";
-      LC_TIME = "es_UY.UTF-8";
-    };
 
     inputMethod = {
       type = "fcitx5";
