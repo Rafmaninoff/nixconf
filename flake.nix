8a44a3605086eaa8e65d7fb8def4a4fd9e7d5e48
Rafael Uria <rafmaninoff@gmail.com>
2024-09-03 21:03:15 -0300

    Revert "add nur, also format with nixfmt"
    This reverts commit 6c94a386dc035d2f0f1147b290e85990e2b384a4.

--- flake.nix
+++ flake.nix
@@ -6,8 +6,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
-    nur.url = "github:nix-community/nur";
-
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
     #home-manager.url = "github:nix-community/home-manager/release-23.05";
@@ -17,6 +15,7 @@
     nixos-hardware.url = "github:nixos/nixos-hardware";
     rust-overlay.url = "github:oxalica/rust-overlay";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+
 
     arrpc = {
       url = "github:notashelf/arrpc-flake";
@@ -39,53 +38,32 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-stable,
-      chaotic,
-      nixos-cli,
-      home-manager,
-      nixos-hardware,
-      rust-overlay,
-      nur,
-      ...
-    }@inputs:
-    let
-      inherit (self) outputs;
+    { self, nixpkgs, nixpkgs-stable, chaotic, nixos-cli, home-manager, nixos-hardware, rust-overlay, ... }@inputs:
+    let inherit (self) outputs;
       system = "x86_64-linux";
-      pkgs-stable = import nixpkgs-stable {
-        inherit system;
-        config = {
-          allowUnfree = true;
-          AllowUnfreePredicate = (_: true);
-        };
-      };
-      overlays = [ rust-overlay.overlays.default ];
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-stable = import nixpkgs-stable { inherit system; config = { allowUnfree = true; AllowUnfreePredicate = (_: true); }; };
+
+      overlays = [
+        rust-overlay.overlays.default
+      ];
     in
     {
       # NixOS configuration entrypoint
       # Available through 'nixos-rebuild --flake .#your-hostname'
       nixosConfigurations = {
         "raf-x570" = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs;
-          };
-          specialArgs = {
-            inherit pkgs-stable;
-          };
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs outputs; };
+          specialArgs = { inherit pkgs-stable; };
           # > Our main nixos configuration file <
           modules = [
             ./hosts/raf-x570/configuration.nix
             chaotic.nixosModules.default
             nixos-cli.nixosModules.nixos-cli
-            (
-              { pkgs, ... }:
-              {
-                nixpkgs.overlays = overlays;
-              }
-            )
-            nur.nixosModules.nur
+            ({ pkgs, ... }: {
+              nixpkgs.overlays = overlays;
+            })
           ];
         };
       };
@@ -94,19 +72,15 @@
       # Available through 'home-manager --flake .#your-username@your-hostname'
       homeConfigurations = {
         "raf@raf-x570" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
+          pkgs =
+            nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
           # > Our main home-manager configuration file <
           modules = [
             ./home/home.nix
-            (
-              { pkgs, ... }:
-              {
-                nixpkgs.overlays = overlays;
-              }
-            )
+            ({ pkgs, ... }: {
+              nixpkgs.overlays = overlays;
+            })
           ];
         };
       };
