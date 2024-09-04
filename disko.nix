4d9e8c74179e677df1366cdb8aa712979564772c
Rafael Uria <rafmaninoff@gmail.com>
2024-09-03 21:03:15 -0300

    Revert "remove bad attempt at disko"
    This reverts commit c3bde647966b7396683efd98a028974bebe72bef.

---
+++ disko.nix
@@ -0,0 +1,48 @@
+{ device ? throw, ... }: {
+  disko.devices = {
+    disk.main = {
+      inherit device;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          esp = {
+            name = "nixosESP";
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          luks-nixos = {
+            size = "100%";
+            name = "nixos-cryptroot";
+            settings = {
+              allowDiscards = true;
+            };
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "subvol=nix" "compress=zstd" "noatime" ];
+                };
+                "/persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = [ "subvol=persist" "compress=zstd" "noatime" ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
