75e0ad7bec5d24b4dd523d70e42df5bf0ff3a6f5
Rafael Uria <rafmaninoff@gmail.com>
2024-09-04 06:53:55 -0300

    reorganise

--- nixosModules/desktop/default.nix
+++ nixosModules/desktop/default.nix
@@ -6,11 +6,57 @@
     ./discord.nix
     ./gaming.nix
     ./fcitx.nix
-    ./desktopPkgs.nix
   ];
 
-  has.discord = lib.mkDefault true;
-  has.waydroid = lib.mkDefault true;
-  has.gaming = lib.mkDefault true;
-  has.fcitx = lib.mkDefault true;
+  options.has.defaultDesktopPackages = lib.mkOption {
+    description = "";
+    type = lib.types.bool;
+    default = true;
+  };
+
+
+  config = lib.mkMerge [
+    {
+      has.discord = lib.mkDefault true;
+      has.waydroid = lib.mkDefault true;
+      has.gaming = lib.mkDefault true;
+      has.fcitx = lib.mkDefault true;
+    }
+    (lib.mkIf config.has.defaultDesktopPackages {
+      environment.systemPackages =
+        (with pkgs; [
+          sops
+          vim
+          age
+          gnome-disk-utility
+          deluge
+          wget
+          flameshot
+          filelight
+          clac
+          vlc
+          piper
+          signal-desktop
+          telegram-desktop
+          zapzap
+          nchat
+          scrcpy
+          rnote
+          pciutils
+          usbutils
+          stremio
+          btrfs-assistant
+          quickemu
+          floorp
+          firefox
+          microsoft-edge
+        ])
+        ++ (with pkgs-stable; [
+          libreoffice-qt
+          hunspell
+          hunspellDicts.es_ES
+          hunspellDicts.en_GB-ise
+        ]);
+    })
+  ];
 }
