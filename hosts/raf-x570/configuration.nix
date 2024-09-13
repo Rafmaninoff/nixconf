99208d5e81a3c5151efe4c4bdfec731364324bce
Rafael Uria <rafmaninoff@gmail.com>
2024-09-13 11:02:04 -0300

    cleanup

--- hosts/raf-x570/configuration.nix
+++ hosts/raf-x570/configuration.nix
@@ -27,13 +27,7 @@
   has.javawrappers = true;
 
   networking.hostName = "raf-x570"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -50,21 +44,12 @@
   programs.hyprland = { enable = true; };
 
 
-  # Enable CUPS to print documents.
-  services.printing.enable = false;
-
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
 
-  # services.blueman.enable = true;
-
   services.flatpak.enable = true;
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.raf = {
     isNormalUser = true;
     description = "raf";
