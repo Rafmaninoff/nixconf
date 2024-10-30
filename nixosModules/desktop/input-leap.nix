7bb1cc5c87d3c6fa6ebdd376fb465d1b2fec3bc1
Rafael Uria <rafmaninoff@gmail.com>
2024-10-30 12:36:35 -0300

    switch to custom fork of input-leap to fix segfault

--- nixosModules/desktop/input-leap.nix
+++ nixosModules/desktop/input-leap.nix
@@ -11,7 +11,19 @@ in
   };
 
   config = mkIf cfg {
-    environment.systemPackages = [ pkgs.input-leap ];
+    environment.systemPackages = [
+      (pkgs.input-leap.overrideAttrs
+        {
+          src = pkgs.fetchFromGitHub {
+            owner = "rafmaninoff";
+            repo = "input-leap";
+            rev = "v3.0.2-qt6.8";
+            hash = "sha256-D4hXjcMOOc+l7XSW/AfBO5rx3sE5zxuhPf2HJyAJrjE=";
+            fetchSubmodules = true;
+          };
+        })
+
+    ];
     networking.firewall = {
       allowedTCPPorts = [ 24800 ];
       allowedUDPPorts = [ 24800 ];
