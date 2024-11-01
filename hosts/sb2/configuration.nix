9155be6c1499640b2ee10a9fcc510b41ed6f2013
Rafael Uria <rafmaninoff@gmail.com>
2024-11-01 15:10:04 -0300

    required option was not enabled

--- hosts/sb2/configuration.nix
+++ hosts/sb2/configuration.nix
@@ -69,6 +69,8 @@
     extraGroups = [ "networkmanager" "wheel" "input" "adbusers" "surface-control" ];
   };
 
+  programs.zsh.enable = true;
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
