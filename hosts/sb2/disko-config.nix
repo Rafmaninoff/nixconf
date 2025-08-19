2e52e97fd566248f7d3ea194c334e68e25fb3a25
Rafael Uria <rafmaninoff@gmail.com>
2025-08-19 17:04:45 -0300

    actually needs devicename

--- hosts/sb2/disko-config.nix
+++ hosts/sb2/disko-config.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "INVALID";
+        device = "/dev/nvme0n1";
         content = {
           type = "gpt";
           partitions = {
