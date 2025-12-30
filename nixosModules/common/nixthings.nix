b4fe081744d421bf0c4bc13d164b1d44b4b813f2
Rafael Uria <rafmaninoff@gmail.com>
2025-12-30 19:48:44 -0300

    reenable nixos-cli

--- nixosModules/common/nixthings.nix
+++ nixosModules/common/nixthings.nix
@@ -5,9 +5,11 @@
     settings.experimental-features = [ "nix-command" "flakes" "pipe-operator" ];
     settings.substituters = [
       "https://watersucks.cachix.org"
+      "https://attic.xuyh0120.win/lantian"
     ];
     settings.trusted-public-keys = [
       "watersucks.cachix.org-1:6gadPC5R8iLWQ3EUtfu3GFrVY7X6I4Fwz/ihW25Jbv8="
+      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
     ];
     optimise = {
       automatic = true;
@@ -33,7 +35,7 @@
     ];
   };
 
-  # services.nixos-cli = {
-  #   enable = true;
-  # };
+  services.nixos-cli = {
+    enable = true;
+  };
 }
